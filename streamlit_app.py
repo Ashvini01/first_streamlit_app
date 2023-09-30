b32fa76361ab1e7d7d1e9f2326ef3fe8c773cafe
Ashvini01 <146530005+Ashvini01@users.noreply.github.com>
2023-09-30 16:55:55 +0530

    Update streamlit_app.py
    pandas step started- import pandas and next line

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,4 +9,6 @@ streamlit.text('🥗 Kale, spinach, & Rocket Smoothie')
 streamlit.text('🐔 Hard Boiled Free range Eggs')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+import pandas 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
